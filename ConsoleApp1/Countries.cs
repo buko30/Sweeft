6ba56db515efa14de456cc46a2a4fb3c590652f5
buko30 <giorgibukia31@gmail.com>
2024-03-02 17:38:13 +0400

    Add project to git

---
+++ ConsoleApp1/Countries.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Countries
+    {
+    static async Task Main(string[] args)
+    {
+        string url = "https://restcountries.com/v3.1/all";
+
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // Deserialize the JSON response
+                var countries = JsonSerializer.Deserialize<Country[]>(responseBody);
+
+                // Create .txt document for each country
+                foreach (var country in countries)
+                {
+                    string fileName = $"{country.Name.Official}.txt";
+                    using (StreamWriter writer = File.CreateText(fileName))
+                    {
+                        writer.WriteLine($"Region: {country.Region}");
+                        writer.WriteLine($"Subregion: {country.Subregion}");
+                        writer.WriteLine($"Latitude: {string.Join(", ", country.Latlng)}");
+                        writer.WriteLine($"Area: {country.Area}");
+                        writer.WriteLine($"Population: {country.Population}");
+                    }
+                }
+
+                Console.WriteLine("Text files created successfully!");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Failed to retrieve data: {ex.Message}");
+            }
+        }
+    }
+
+    public class Country
+    {
+        public OfficialName Name { get; set; }
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public double[] Latlng { get; set; }
+        public double Area { get; set; }
+        public int Population { get; set; }
+    }
+
+    public class OfficialName
+    {
+        public string Official { get; set; }
+    }
+}
+
+    }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    //     1
+    public static bool checkPalindrome(string text)
+    {
+        char[] c = text.ToCharArray();
+        Array.Reverse(c);
+        return new string(c).Equals(text);
+    }
+
+
+
+    //     2
+    public static int MinSplit(int amount)
+    {
+        int[] coins = [50, 20, 10, 5, 1];
+        int counter = 0;
+        foreach (int i in coins)
+        {
+            if (i > amount)
+            {
+                continue;
+            }
+            else
+            {
+                counter += amount / i;
+                amount = amount % i;
+            }
+        }
+        return counter;
+    }
+
+
+    //        3
+    public static int NotContains(int[] array)
+    {
+        HashSet<int> noDuplicates = new HashSet<int>(array);
+        int min = 1;
+        while (noDuplicates.Contains(min))
+        {
+            min += 1;
+        }
+        return min;
+    }
+
+
+
+    //        4
+    public static bool IsProperly(string sequence)
+    {
+        int counter = 0;
+        foreach (char c in sequence)
+        {
+            if (c == '(')
+            {
+                counter++;
+            }
+            else if (c == ')')
+            {
+                counter--;
+                if (counter < 0)
+                {
+                    return false;
+                }
+            }
+        }
+        return counter == 0;
+    }
+
+    public static int countCombinations(int n)
+    {
+        if (n <= 0)
+        {
+            return 1;
+        }
+        else if(n == 1)
+        {
+            return 1;
+        }
+        else if(n == 2)
+        {
+            return 2;
+        }
+        else
+        {
+            return countCombinations(n - 1) + countCombinations(n - 2);
+        }
+    }
+
+
+    private static void Main(string[] args)
+    {
+ 
+        Console.WriteLine(countCombinations(6));
+    }
+}
